06aa5015f0ec0c4e5ed65cba18eedcf2d0b9ee8e
elioye27 <elioye27@gmail.com>
2017-11-26 22:52:36 -0800

    add files

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
